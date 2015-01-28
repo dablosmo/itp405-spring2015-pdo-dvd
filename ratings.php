b0a53b4e1feb5076ab859264b02278d2bf7296ac
David Zhang <David@Davids-MacBook-Pro.local>
2015-01-27 17:32:13 -0800

    Basic functionality complete, may need more visual touch ups

---
+++ ratings.php
@@ -0,0 +1,38 @@
+<?php 
+
+$rating = $_GET['rating']; 
+
+?>
+
+<h3>
+<?php echo "Movies that are rated " . $rating; ?>
+</h3>
+
+<?php
+
+$host = 'itp460.usc.edu'; 
+$dbname = 'dvd'; 
+$user = 'student'; 
+$password = 'ttrojan'; 
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); 
+
+$sql = " 
+	SELECT title, rating_name 
+	FROM dvds  
+	INNER JOIN ratings 
+	ON ratings.id = dvds.rating_id
+	WHERE ratings.rating_name = ?
+";
+
+$statement = $pdo->prepare($sql);
+$statement->bindParam(1, $rating);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_OBJ);
+
+foreach($results as $result) : ?> 
+	<div> 
+		<?php echo $result->title ?> 
+	</div>
+<?php endforeach ?> 
+
---
+++ results.php
@@ -0,0 +1,68 @@
+<?php 
+
+if (!isset($_GET['title'])) { 
+	header("Location: index.php");
+}
+
+$title = $_GET['title'];
+
+?> 
+
+<h3> 
+You searched for '<?php echo $title ?>'
+</h3>
+
+<?php
+$host = 'itp460.usc.edu'; 
+$dbname = 'dvd'; 
+$user = 'student'; 
+$password = 'ttrojan'; 
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); 
+
+$sql = " 
+	SELECT title, genre_name, format_name, rating_name 
+	FROM dvds 
+	INNER JOIN genres 
+	ON dvds.genre_id = genres.id 
+	INNER JOIN formats 
+	ON formats.id = dvds.format_id 
+	INNER JOIN ratings 
+	ON ratings.id = dvds.rating_id
+	WHERE title LIKE ?
+";
+
+$statement = $pdo->prepare($sql);
+$like = '%' . $title . '%'; 
+$statement->bindParam(1, $like);
+$statement->execute(); 
+$dvds = $statement->fetchAll(PDO::FETCH_OBJ); 
+// var_dump($dvds); 
+
+// foreach($songs as $song) { 
+// 	echo "<div>" . $song['title'] . "</div>"; 
+// 	echo "<br>"; 
+// }
+
+?> 
+
+<?php 
+	if(empty($dvds)) { 
+		echo "No Results Found.";
+?> 
+		<form><input type = "button" value = "Back" 
+		onclick ="history.go(-1);return true;"></form>
+
+<?php 
+
+	} 
+
+foreach($dvds as $dvd) : ?> 
+	<h3> 
+		<?php echo $dvd->title ?> 
+	</h3>
+	<p> Genre: <?php echo $dvd->genre_name ?> </p>
+	<p> Format:	<?php echo $dvd->format_name ?> </p>
+	<p>	Rating: <a href="ratings.php?rating=<?php echo $dvd->rating_name ?>">
+			<?php echo $dvd->rating_name ?></a> </p>
+<?php endforeach ?> 
